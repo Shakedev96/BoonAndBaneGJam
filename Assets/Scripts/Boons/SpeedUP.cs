9375cba1c26793a0f1b9cffbb95adf2fa94324de
Shakedev96 <99559163+Shakedev96@users.noreply.github.com>
2025-02-07 11:54:10 +0530

    comit

---
+++ Assets/Scripts/Boons/SpeedUP.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUP : MonoBehaviour
+{
+    public float duration = 5f;  // Boost duration
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMove player = other.GetComponent<PlayerMove>();
+            if (player != null)
+            {
+                player.ApplySpeedBoost();
+            }
+
+            // Disable power-up after pickup
+            gameObject.SetActive(false);
+        }
+    }
+}
+/*
+using UnityEngine;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    public float duration = 5f;  // Boost duration
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMove player = other.GetComponent<PlayerMove>();
+            if (player != null)
+            {
+                player.ApplySpeedBoost();
+            }
+
+            // Disable power-up after pickup
+            gameObject.SetActive(false);
+        }
+    }
+}
+
+
+*/
--- Assets/Scripts/PLayer/PlayerJump.cs
+++ Assets/Scripts/PLayer/PlayerJump.cs
@@ -10,26 +10,22 @@ public class PlayerJump : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.3f;
     public LayerMask groundLayer;
-
-    // new change
-    // [SerializeField] private float jumpAmount;
-    // [SerializeField] private float maxDuration;
-
-    // [SerializeField] private float jumpTime;
-    // [SerializeField] private bool jumping;
-
-    [SerializeField] private float gravityScale;
-    [SerializeField] private PlayerInput playerInput;
-
-    // [SerializeField] private float currentGravityScale;
-    // [SerializeField] private float fallingGravityScale;
-    
-    private Rigidbody rb;
+    public float gravityScale = 1f;
     public bool isGrounded;
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+
+    [Header("Jump Logic")]
+    public int maxJumps = 1;  // Default jump count
+    private int currentJumps = 0;
+
+    [Header("Power-Up Settings")]
+    public int boostedJumpCount = 2;   // Double jump
+    public float powerUpDuration = 5f; // Duration of double jump effect
 
     void Awake()
     {
-        PlayerInput playerInput = GetComponent<PlayerInput>();
+        playerInput = GetComponent<PlayerInput>();
 
         if (Gamepad.current != null)
         {
@@ -40,9 +36,6 @@ public class PlayerJump : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-       // Physics.gravity *= gravityMod;
-       playerInput = GetComponent<PlayerInput>();
-
 
         InputAction jumpAction = playerInput.actions.FindAction("Jump");
         if (jumpAction != null)
@@ -53,19 +46,15 @@ public class PlayerJump : MonoBehaviour
         {
             Debug.LogError("Jump action not found in Input Action Asset!");
         }
-
-
-       
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(context.performed && isGrounded)
+        if (context.performed && currentJumps < maxJumps)
         {
             Debug.Log($"Jump button pressed from: {context.control.device.displayName}");
             Jump();
         }
-        Debug.Log("X is pressed");
     }
 
     void FixedUpdate()
@@ -76,29 +65,46 @@ public class PlayerJump : MonoBehaviour
     void Update()
     {
         CheckGround();
-
-        /* if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            Jump();
-            
-        } */
-
-       
     }
 
     private void CheckGround()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
-        Debug.Log("Grounded");
+        if (Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer))
+        {
+            isGrounded = true;
+            currentJumps = 0; // Reset jump count when grounded
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
     private void Jump()
     {
         isGrounded = false;
-        float jumpForce = Mathf.Sqrt(jumpHeight * -3 * (Physics.gravity.y * gravityScale));
+        currentJumps++; // Increase jump count
 
+        float jumpForce = Mathf.Sqrt(jumpHeight * -3 * (Physics.gravity.y * gravityScale));
         rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-        //rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    // 🚀 **Apply Double Jump Power-Up**
+    public void ApplyDoubleJumpPowerUp()
+    {
+        StartCoroutine(DoubleJumpRoutine());
+    }
+
+    private IEnumerator DoubleJumpRoutine()
+    {
+        maxJumps = boostedJumpCount; // Enable double jump
+        yield return new WaitForSeconds(powerUpDuration);
+        maxJumps = 1; // Reset to normal jump
     }
 
 }
+/*
+
+}
+
+*/
